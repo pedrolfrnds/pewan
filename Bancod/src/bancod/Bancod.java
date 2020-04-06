a637dca7e915b060349742dc330a8c8ec4e58756
Idaslon <destroyeeru18@gmail.com>
2020-04-06 11:48:24 -0300

    Project inital commit

---
+++ Bancod/src/bancod/Bancod.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bancod;
+import java.io.File;
+/**
+ *
+ * @author wanessa Nyedja
+ */
+public class Bancod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
